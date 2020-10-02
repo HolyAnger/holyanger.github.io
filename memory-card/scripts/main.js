2ed69f19424896afa695aa95e86e1255951792ca
Oleg Mischenko <mischenko.oleg.mikhailovich@gmail.com>
2020-10-02 17:55:04 +0300

    Update main.js

--- memory-card/scripts/main.js
+++ memory-card/scripts/main.js
@@ -1,7 +1,5 @@
     const config = {
       scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1200,
         height: 700,
         scene: new GameScene(),
