669b006edb3f192a2d813ea016899b6535370bab
Oleg Mischenko <mischenko.oleg.mikhailovich@gmail.com>
2020-10-02 17:52:15 +0300

    Update main.js

--- memory-card/scripts/main.js
+++ memory-card/scripts/main.js
@@ -1,7 +1,17 @@
-const config = {
-  width: 1280,
-  height: 720,
-  scene: new GameScene(),
-};
+window.addEventListener('load', () => {
+  window.setTimeout(() => {
+    const DEFAULT_HEIGHT = 720
+    const DEFAULT_WIDTH = (window.innerWidth / window.innerHeight) * DEFAULT_HEIGHT
 
-const game = new Phaser.Game(config);
+    const config = {
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
+        scene: new GameScene(),
+      },
+    }
+    new Phaser.Game(config)
+  }, 1000)
+});
